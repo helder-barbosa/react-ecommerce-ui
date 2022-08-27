44a512a8735a4f3701c0723e6fc47fd1e6b368ca
Helder Barbosa <helderbarbosa.dev@gmail.com>
2022-08-26 23:27:21 -0300

    #3 Newsletter / Footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Announcement from '../components/Announcement';
 import Categories from '../components/Categories';
+import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
+import Newsletter from '../components/Newsletter';
 import ProductList from '../components/ProductList';
 import Slider from '../components/Slider';
 
@@ -13,6 +15,8 @@ const Home = () => {
       <Slider />
       <Categories />
       <ProductList />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
