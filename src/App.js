7c589eb1b7a1409656fb7f9284a4ccf2d53fbf7b
Helder Barbosa <helderbarbosa.dev@gmail.com>
2022-09-02 21:34:29 -0300

    #7 Cart Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Login from './pages/Login';
+import Cart from './pages/Cart';
+// import Login from './pages/Login';
 // import Product from './pages/Product';
 // import Register from './pages/Register';
 // import ProductList from './pages/ProductList';
 
 function App() {
-  return <Login />;
+  return <Cart />;
 }
 
 export default App;
